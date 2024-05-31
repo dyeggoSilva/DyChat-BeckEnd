133ce22c5a785407f7c709a9ce8044d947485707
Dyeggo Silva <dyeggo0102@live.com>
2024-05-31 01:13:33 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
-const {WebSocketServer} =require ("ws")
-const dotenv =require ("dotenv")
+const {WebSocketServer} = require ("ws")
+const dotenv = require ("dotenv")
 
 dotenv.config()
 
-const wss = new WebSocketServer({port: process.env.PORT || 8080})
+const wss = new WebSocketServer({port: 8080})
 
-wss.on (" connection", (ws)=> {
+wss.on ("connection", (ws)=> {
     ws.on("error", console.error)
 
-    ws.on("mensage", (data)=>{
+    ws.send("mensagem enviada do server")
+
+    ws.on("message", (data)=>{
+        
         wss.clients.forEach((client) => client.send(data.toString()))
     })
     console.log("client conected")
